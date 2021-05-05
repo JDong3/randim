8fe6bab3d7262aebc125bf47dae68843b9777493
xxcandiez <josephdong1998@gmail.com>
2021-05-05 18:03:48 -0400

    xd

--- test.py
+++ test.py
@@ -4,20 +4,24 @@ import random
 random.seed(100)
 
 class ImageGenerator:
-    def __init__(self, res=(1920, 1080)):
-        self.img = Image.new('RGB', res, 'black')
+    def __init__(self, res=(1920, 1080), default_name='xd'):
+        self.name_root = 'xd'
+        self.resolution = res
+
+    def create_baseline(self):
+        return Image.new('RGB', res, 'black')
 
     def create_random(self):
+        img = Image.new( 'RGB', (500,500), "black")
+        pixels = img.load()
+        for i in range(img.size[0]):
+            for j in range(img.size[1]):
+                rands = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+                pixels[i,j] = rands
         pass
 
-img = Image.new( 'RGB', (500,500), "black") # Create a new black image
-pixels = img.load() # Create the pixel map
-for i in range(img.size[0]):    # For every pixel:
-    for j in range(img.size[1]):
-        rands = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
-        # xd = random.randint(0, 255)
-        # rands = (xd, xd, xd)
-        pixels[i,j] = rands # Set the colour accordingly
+
+
 
 # img.save('xd2.bmp', format='bmp')
 img.show()
