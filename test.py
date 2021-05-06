380d19347835d8b83866fad1a279aba2877ae55c
Simon Dickenson <mtwangyugeng@gmail.com>
2021-05-05 20:44:57 -0400

    added create_square

--- test.py
+++ test.py
@@ -4,12 +4,12 @@ import random
 random.seed(100)
 
 class ImageGenerator:
-    def __init__(self, res=(1920, 1080), default_name='xd'):
+    def __init__(self, res=(1920, 1080), default_name='pipi'):
         self.name_root = 'xd'
         self.resolution = res
 
     def create_baseline(self):
-        return Image.new('RGB', res, 'black')
+        return Image.new('RGB', self.resolution, 'black')
 
     def create_random(self):
         img = Image.new( 'RGB', (500,500), "black")
@@ -19,9 +19,27 @@ class ImageGenerator:
                 rands = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
                 pixels[i,j] = rands
         pass
+    def create_squares(self,n):
+        img = Image.new( 'RGB', self.resolution, "black")
+        pixels = img.load()
+        i = 0
+        j = 0
+        while j < img.size[1]:
+            rands = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+            for ii in range(i, i + n):
+                if ii >= img.size[0]:
+                    break
+                for jj in range(j, j + n):
+                    if jj >= img.size[1]:
+                        break
+                    pixels[ii,jj] = rands
+            i = i + n
+            if i >= img.size[0]:
+                i = 0
+                j = j + n
+        return img      
 
 
 
-
-# img.save('xd2.bmp', format='bmp')
-img.show()
+ii = ImageGenerator()
+ii.create_squares(100).save('xd2.bmp', format='bmp')
